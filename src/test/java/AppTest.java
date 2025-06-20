01bfa6e20f93008d9f0a1c2fd8420d4dbc657218
Umut Ay <umutaybora@gmail.com>
2025-06-20 14:18:09 +0200

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,6 +1,7 @@
 import com.fasterxml.jackson.core.JsonProcessingException;
 import context.ContextStore;
 import models.Pet;
+import okhttp3.Headers;
 import ollama.Ollama;
 import ollama.models.inference.InferenceModel;
 import org.junit.Test;
@@ -15,11 +16,14 @@ import java.util.List;
 public class AppTest {
 
     Printer log = new Printer(AppTest.class);
+    String ollamaToken = "sk-88c6492c98af436f8de8b40182f63c8a";
 
     @Test
     public void generationTest() throws JsonProcessingException {
         ContextStore.loadProperties("test.properties");
-        Ollama ollama = new Ollama("http://i-bora.com:11435/");
+        Ollama ollama = new Ollama(
+                "https://i-bora.com/ollama/",
+                Headers.of("Authorization", "Bearer " + ollamaToken));
         InferenceModel prompt = new InferenceModel.Builder()
                 .model("gemma3:27b")
                 .prompt("Create a pet. leave ID null")
